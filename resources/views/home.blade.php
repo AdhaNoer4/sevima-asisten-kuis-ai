a8e0600bbdc1fbca25106f10a10787516b2a1e36
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2023-06-24 11:03:36 +0700

    membuat halaman home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('template.main')
+
+@section('container')
+<div class="col-lg-8">
+    <div class="mt-10">
+        <h3>Selamat Datang!</h3>
+        <h1>KuisAsist.AI</h1>
+        <p>Uji pemahamanmu tentang materi pelajaran di sekolah</p>
+    </div>
+    <div class="">
+        <h4>Klik Start untuk mulai!</h4>
+        <a href="" class="btn btn-outline-primary mt-3">Start!</a>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+//  Halaman Home
+Route::get('/', [HomeController::class, 'index']);
+
+// Halaman About
+Route::get('/about', [HomeController::class, 'index']);
+
+// Halaman Category
+Route::get('/categories', [HomeController::class, 'index']);
+
+// Halaman Login
+Route::get('/login', function () {
+    return view('login');
 });
